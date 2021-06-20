f3fea4f3aa796c5a492b4cff2f87b39c4fb86d95
Ivana Marosevic <i.marosevic@hotmail.com>
2021-06-20 14:21:06 +0200

    Initialized tennis-court and tennis-player microservices

---
+++ tennis-player/src/main/java/com/code9/tennisplayer/TennisPlayerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.code9.tennisplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisPlayerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisPlayerServiceApplication.class, args);
+	}
+
+}
---
+++ tennis-player/src/test/java/com/code9/tennisplayer/TennisPlayerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code9.tennisplayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisPlayerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
