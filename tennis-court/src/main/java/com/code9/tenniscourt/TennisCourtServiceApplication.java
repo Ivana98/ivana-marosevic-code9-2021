22686184ab65604ac378a8ded1e63d816e6681ff
Ivana Marosevic <i.marosevic@hotmail.com>
2021-06-30 14:25:31 +0200

    Added eureka server and clients

--- tennis-court/src/main/java/com/code9/tenniscourt/TennisCourtServiceApplication.java
+++ tennis-court/src/main/java/com/code9/tenniscourt/TennisCourtServiceApplication.java
@@ -2,8 +2,10 @@ package com.code9.tenniscourt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TennisCourtServiceApplication {
 
 	public static void main(String[] args) {
--- tennis-player/src/main/java/com/code9/tennisplayer/TennisPlayerServiceApplication.java
+++ tennis-player/src/main/java/com/code9/tennisplayer/TennisPlayerServiceApplication.java
@@ -2,8 +2,10 @@ package com.code9.tennisplayer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TennisPlayerServiceApplication {
 
 	public static void main(String[] args) {
